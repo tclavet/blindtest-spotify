656c550db8a86fa827ba12d069f92730f6129e74
Thomas Clavet <thomas.clavet@sciencespo.fr>
2018-10-24 20:11:36 +0200

    Updates

---
+++ precache-manifest.17f0079279fc503faa57385797116399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "52d7ca60280c720b3968",
+    "url": "/blindtest-spotify/static/js/main.52d7ca60.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "52d7ca60280c720b3968",
+    "url": "/blindtest-spotify/static/css/main.07a167d9.chunk.css"
+  },
+  {
+    "revision": "22be73bc01c020ba41459071c9908659",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
